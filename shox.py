b6f70a03b2431dcbfcea13f75329331c958a8f91
devz0505-blip <devz0505@gmail.com>
2026-07-06 16:02:28 +0200

    Update shox.py

--- shox.py
+++ shox.py
@@ -65,13 +65,13 @@ if release_id == last_id:
 
 embed = {
     "title": "🔥 Neue Release von SHOX!",
-    "description": f"## 🎵 {name}\nNeue Single von **SHOX** ist jetzt auf Spotify verfügbar.",
+    "description": f"🎵 **{name}**\n\nEine neue {album_type.lower()} von **SHOX** ist jetzt auf Spotify verfügbar."
     "url": url,
     "color": 0x8A2BE2,
 
    "author": {
     "name": "SHOX",
-    "icon_url": "https://cdn.discordapp.com/attachments/734182009618038856/1523689983448318232/WhatsApp_Image_2026-05-11_at_01.38.04.jpeg?ex=6a4d0660&is=6a4bb4e0&hm=900c3e78021df6f8995e8f731062ae6c2b277daf65e5b484d23d92684506257a&"
+    "icon_url": "  "
 },
 
     "thumbnail": {
