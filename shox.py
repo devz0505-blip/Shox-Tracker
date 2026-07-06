d6cecfb7f0d9fa5fc59816629094807649f9ba1f
devz0505-blip <devz0505@gmail.com>
2026-07-06 15:20:58 +0200

    Update shox.py

--- shox.py
+++ shox.py
@@ -4,7 +4,7 @@ import os
 
 WEBHOOK = "https://discord.com/api/webhooks/1511520888963465376/WHvr_TSWrabk0H8X4k4J0INg6T1JIulx--DvM3ZA2Gls1Gg5IgHK6SgcCVgW1aHYqw0m"
 
-ARTIST_ID = "4qemKfJpXP6zS0OWvWUQbZ"
+ARTIST_ID = "4qemKfpJPX6zS00wWwUQbZ"
 
 CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
 CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
@@ -15,7 +15,12 @@ def get_token():
         data={"grant_type": "client_credentials"},
         auth=(CLIENT_ID, CLIENT_SECRET)
     )
-    return r.json()["access_token"]
+    print(r.status_code)
+print(r.text)
+
+r.raise_for_status()
+
+return r.json()["access_token"]
 
 token = get_token()
 
@@ -27,6 +32,8 @@ r = requests.get(
     f"https://api.spotify.com/v1/artists/{ARTIST_ID}/albums?include_groups=single,album",
     headers=headers
 )
+print(r.status_code)
+print(r.text)
 
 latest = r.json()["items"][0]
 
